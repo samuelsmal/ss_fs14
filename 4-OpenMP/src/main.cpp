26e3c29f031341fccb00cc81253daafeff535bc1
Samuel von Baussnern <samuel.vonbaussnern@gmail.com>
2014-11-05 02:03:36 +0100

    basic serial algorithm

--- 4-OpenMP/src/main.cpp
+++ 4-OpenMP/src/main.cpp
@@ -17,6 +17,8 @@
 #include <iterator>
 #include <algorithm>
 #include <sstream>
+#include <map>
+#include <utility> // make_pair
 
 #include "util.h"
 
@@ -36,6 +38,32 @@ int main(int argc, const char* argv[])
   //size_t number_of_threads = parseSizeTArgument(argv[3]);
   //short execution_mode = parseShortArgument(argv[4]);
 
+  map<size_t, vector<size_t>> occurrences;
+
+  for (size_t pats_index = 0; pats_index < patterns.size(); ++pats_index) {
+    string pat = patterns.at(pats_index);
+    for (size_t s = 0; s < sequence.length(); ++s) {
+      for (size_t c_s = s, pattern_index = 0; (c_s < sequence.length()) && (pattern_index < pat.length()); ++c_s, ++pattern_index) {
+        if (sequence.at(c_s) != pat.at(pattern_index)) {
+          break;
+        } else if (pattern_index == pat.length() - 1) {
+          if (occurrences.find(pats_index) == occurrences.end()) {
+            occurrences.emplace(pats_index, vector<size_t>{s});
+          } else {
+            occurrences.find(pats_index)->second.emplace_back(s);
+          }
+        }
+      }
+    }
+  }
+
+  for (const auto &occ : occurrences) {
+    for (const auto &match : occ.second) {
+      cout << "(" << occ.first << ", " << match << ") ";
+    }
+  }
+
+  cout << endl;
 
   return 0;
 }
