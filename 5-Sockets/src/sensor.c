861afa77abcb67ee06bc120a7a1597e2065a7834
Samuel von Baussnern <samuel.vonbaussnern@gmail.com>
2014-11-22 22:32:56 +0100

    converts tabs to spaces

--- 5-Sockets/src/sensor.c
+++ 5-Sockets/src/sensor.c
@@ -25,93 +25,93 @@ static const char *SOCKET_ADDRESS;
 static int SERVER_SOCKET;
 
 int main(int argc, const char* argv[]) {
-	if (argc != 2) {
-		const char err_msg[] = "Wrong number of arguments";
-		const char usage_msg[] = "Usage: sensor <String: socket address>";
-		printf("%s\n%s\n", err_msg, usage_msg);
-		exit(EXIT_FAILURE);
-	}
+  if (argc != 2) {
+    const char err_msg[] = "Wrong number of arguments";
+    const char usage_msg[] = "Usage: sensor <String: socket address>";
+    printf("%s\n%s\n", err_msg, usage_msg);
+    exit(EXIT_FAILURE);
+  }
 
   SOCKET_ADDRESS = argv[1];
 
-	struct sigaction sigIntHandler;
-	sigIntHandler.sa_handler = signalHandler;
-	sigemptyset(&sigIntHandler.sa_mask);
-	sigIntHandler.sa_flags = 0;
-	sigaction(SIGINT, &sigIntHandler, NULL);
+  struct sigaction sigIntHandler;
+  sigIntHandler.sa_handler = signalHandler;
+  sigemptyset(&sigIntHandler.sa_mask);
+  sigIntHandler.sa_flags = 0;
+  sigaction(SIGINT, &sigIntHandler, NULL);
 
-	struct sockaddr_un addr_client;
-	socklen_t addr_client_len;
+  struct sockaddr_un addr_client;
+  socklen_t addr_client_len;
 
-	struct sockaddr_un addr_server;
-	addr_server.sun_family = AF_UNIX;
-	strcpy(addr_server.sun_path, SOCKET_ADDRESS);
-	socklen_t
-	addr_server_len = offsetof( struct sockaddr_un, sun_path ) + strlen( addr_server.sun_path );
+  struct sockaddr_un addr_server;
+  addr_server.sun_family = AF_UNIX;
+  strcpy(addr_server.sun_path, SOCKET_ADDRESS);
+  socklen_t
+  addr_server_len = offsetof( struct sockaddr_un, sun_path ) + strlen( addr_server.sun_path );
 
   unlink(argv[1]);
 
-	if ((SERVER_SOCKET = initserver(SOCK_STREAM,
-			(struct sockaddr*) &addr_server, addr_server_len, 10)) < 0) {
-		exit(EXIT_FAILURE);
-	}
+  if ((SERVER_SOCKET = initserver(SOCK_STREAM,
+      (struct sockaddr*) &addr_server, addr_server_len, 10)) < 0) {
+    exit(EXIT_FAILURE);
+  }
 
-	while (1) {
-		if (listen(SERVER_SOCKET, 1) < 0) {
-			perror("Error in listen()");
-			exit(EXIT_FAILURE);
-		}
+  while (1) {
+    if (listen(SERVER_SOCKET, 1) < 0) {
+      perror("Error in listen()");
+      exit(EXIT_FAILURE);
+    }
 
-		// ***   accept an incoming connection   ***
+    // ***   accept an incoming connection   ***
 
-		addr_client_len = sizeof(struct sockaddr_un);
-		int client_socket = accept(SERVER_SOCKET,
-				(struct sockaddr*) &addr_client, &addr_client_len);
+    addr_client_len = sizeof(struct sockaddr_un);
+    int client_socket = accept(SERVER_SOCKET,
+        (struct sockaddr*) &addr_client, &addr_client_len);
 
-		if (client_socket < 0) {
-			perror("Error in accept()");
-			exit(EXIT_FAILURE);
-		} else {
-			printf("Connection established! (%d) Waiting for request...\n",
-					client_socket);
-		}
+    if (client_socket < 0) {
+      perror("Error in accept()");
+      exit(EXIT_FAILURE);
+    } else {
+      printf("Connection established! (%d) Waiting for request...\n",
+          client_socket);
+    }
 
-		int temperature = rand() % 99 + 10;
+    int temperature = rand() % 99 + 10;
 
-		pid_t pid = fork();
-		if (pid == -1) {
-			printf("fork error\n");
-			exit(1);
-		} else if (pid == 0) {
-			// ***	child process	***
+    pid_t pid = fork();
+    if (pid == -1) {
+      printf("fork error\n");
+      exit(1);
+    } else if (pid == 0) {
+      // ***  child process ***
 
-			char buf[80];
-			ssize_t nr = read(client_socket, buf, 80);
+      char buf[80];
+      ssize_t nr = read(client_socket, buf, 80);
 
-			buf[nr] = '\0';
-			printf("Received: %s (id = %d)\n", buf, client_socket);
+      buf[nr] = '\0';
+      printf("Received: %s (id = %d)\n", buf, client_socket);
 
-			char output[80];
-			sprintf(output, "%d", temperature);
+      char output[80];
+      sprintf(output, "%d", temperature);
 
-			if (strncmp(buf, AUTH_TOKEN, 3) == 0) {
-				write(client_socket, output, 2);
-				printf("Sent: %s (id = %d)\n", output, client_socket);
-			} else {
-				write(client_socket, "ERR", 3);
-				printf("Sent: ERR (id = %d)\n", client_socket);
-			}
+      if (strncmp(buf, AUTH_TOKEN, 3) == 0) {
+        write(client_socket, output, 2);
+        printf("Sent: %s (id = %d)\n", output, client_socket);
+      } else {
+        write(client_socket, "ERR", 3);
+        printf("Sent: ERR (id = %d)\n", client_socket);
+      }
 
-			// ***   close sockets   ***
+      // ***   close sockets   ***
 
-			close(client_socket);
-			exit(EXIT_SUCCESS);
-		} else {
-			// *** main(parent) process	***
+      close(client_socket);
+      exit(EXIT_SUCCESS);
+    } else {
+      // *** main(parent) process ***
 
-			signal(SIGCHLD, SIG_IGN);
-		}
-	}
+      signal(SIGCHLD, SIG_IGN);
+    }
+  }
   exit(EXIT_SUCCESS);
 }
 
