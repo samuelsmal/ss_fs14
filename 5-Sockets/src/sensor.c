673891d745b25a23aad1b023af2d056b2bd35e05
rbrenn <romanbrenn@gmail.com>
2014-11-22 22:00:33 +0100

    Adds new code.

--- 5-Sockets/src/sensor.c
+++ 5-Sockets/src/sensor.c
@@ -22,56 +22,102 @@
 
 int initserver(int type, const struct sockaddr *addr, socklen_t alen, int qlen);
 
-int main() {
-  // TODO shutdown gracefully
-  // TODO enable mulitiple hosts
+int main(int argc, const char* argv[]) {
+	if (argc != 2) {
+		const char err_msg[] = "Wrong number of arguments";
+		const char usage_msg[] = "Usage: sensor <String: socket address>";
+		printf("%s\n%s\n", err_msg, usage_msg);
+		exit(EXIT_FAILURE);
+	}
 
-  struct sockaddr_un addr_client;
-  socklen_t addr_client_len;
+	struct sigaction sigIntHandler;
+	sigIntHandler.sa_handler = signalHandler;
+	sigemptyset(&sigIntHandler.sa_mask);
+	sigIntHandler.sa_flags = 0;
+	sigaction(SIGINT, &sigIntHandler, NULL);
 
-  struct sockaddr_un addr_server;
-  addr_server.sun_family = AF_UNIX;
-  strcpy( addr_server.sun_path, SOCKET_NAME );
-  socklen_t addr_server_len = offsetof( struct sockaddr_un, sun_path ) + strlen( addr_server.sun_path );
+	struct sockaddr_un addr_client;
+	socklen_t addr_client_len;
 
-  int server_socket;
-  if ( ( server_socket = initserver( SOCK_STREAM, ( struct sockaddr* )&addr_server, addr_server_len, 10) ) < 0) {
-    exit(EXIT_FAILURE);
-  }
+	struct sockaddr_un addr_server;
+	addr_server.sun_family = AF_UNIX;
+	strcpy(addr_server.sun_path, argv[1]);
+	socklen_t
+	addr_server_len = offsetof( struct sockaddr_un, sun_path ) + strlen( addr_server.sun_path );
 
-  if( listen( server_socket, 1 ) < 0 ) {
-    perror("Error in listen()");
-    exit(EXIT_FAILURE);
-  }
+	int server_socket;
+	if ((server_socket = initserver(SOCK_STREAM,
+			(struct sockaddr*) &addr_server, addr_server_len, 10)) < 0) {
+		exit(EXIT_FAILURE);
+	}
 
-  // ***   accept an incoming connection   ***
+	int bool = 1;
 
-  addr_client_len = sizeof( struct sockaddr_un );
-  int client_socket = accept( server_socket, ( struct sockaddr* )&addr_client, &addr_client_len );
+	while (bool == 1) {
+		if (listen(server_socket, 1) < 0) {
+			perror("Error in listen()");
+			exit(EXIT_FAILURE);
+		}
 
-  if( client_socket < 0 ) {
-    perror("Error in accept()");
-    exit(EXIT_FAILURE);
-  }
+		// ***   accept an incoming connection   ***
 
-  char buf[ 80 ];
-  ssize_t nr = read( client_socket, buf, 80 );
+		addr_client_len = sizeof(struct sockaddr_un);
+		int client_socket = accept(server_socket,
+				(struct sockaddr*) &addr_client, &addr_client_len);
 
-  buf[ nr ] = '\0';
-  printf("%s\n", buf);
+		if (client_socket < 0) {
+			perror("Error in accept()");
+			exit(EXIT_FAILURE);
+		} else {
+			printf("Connection established! (%d) Waiting for request...\n",
+					client_socket);
+		}
 
-  if (strncmp(buf, AUTH_TOKEN, 3) == 0) {
-    write( client_socket, "10", 2 );
-  } else {
-    write( client_socket, "ERR", 3 );
-  }
+		int temperature = rand() % 99 + 10;
 
-  // ***   close sockets   ***
+		pid_t pid = fork();
+		if (pid == -1) {
+			printf("fork error\n");
+			exit(1);
+		} else if (pid == 0) {
+			// ***	child process	***
 
-  close( client_socket );
-  close( server_socket );
+			char buf[80];
+			ssize_t nr = read(client_socket, buf, 80);
 
-  unlink( SOCKET_NAME );
+			buf[nr] = '\0';
+			printf("Received: %s (id = %d)\n", buf, client_socket);
 
-  exit(EXIT_SUCCESS);
+			char output[80];
+			sprintf(output, "%d", temperature);
+
+			if (strncmp(buf, AUTH_TOKEN, 3) == 0) {
+				write(client_socket, output, 2);
+				printf("Sent: %s (id = %d)\n", output, client_socket);
+			} else {
+				write(client_socket, "ERR", 3);
+				printf("Sent: ERR (id = %d)\n", client_socket);
+			}
+
+			// ***   close sockets   ***
+
+			close(client_socket);
+			exit(EXIT_SUCCESS);
+		} else {
+			// *** main(parent) process	***
+
+			signal(SIGCHLD, SIG_IGN);
+		}
+	}
+
+	close(server_socket);
+
+	unlink(argv[1]);
+
+	const char end_msg[] = "Press RETURN to exit!";
+	printf("%s\n", end_msg);
+
+	getchar();
+
+	exit(EXIT_SUCCESS);
 }
--- 5-Sockets/src/utils.h
+++ 5-Sockets/src/utils.h
@@ -14,33 +14,35 @@
 #define MAXSLEEP 128
 
 int initserver(int type, const struct sockaddr *addr, socklen_t alen, int qlen);
-int connect_retry(int domain, int type, int protocol, const struct sockaddr *addr, socklen_t alen);
+int connect_retry(int domain, int type, int protocol,
+		const struct sockaddr *addr, socklen_t alen);
+void signalHandler(int signum);
 
-int initserver(int type, const struct sockaddr *addr, socklen_t alen, int qlen) {
-  int fd;
-  int err = 0;
+int initserver(int type, const struct sockaddr *addr, socklen_t alen,
+		int qlen) {
+	int fd;
+	int err = 0;
 
-  if ((fd = socket(addr->sa_family, type, 0)) < 0) {
-    return(-1);
-  }
+	if ((fd = socket(addr->sa_family, type, 0)) < 0) {
+		return (-1);
+	}
 
-  if (bind(fd, addr, alen) < 0) {
-    goto ERROUT_INITSERVER;
-  }
+	if (bind(fd, addr, alen) < 0) {
+		goto ERROUT_INITSERVER;
+	}
 
-  if (type == SOCK_STREAM || type == SOCK_SEQPACKET) {
-    if (listen(fd, qlen) < 0) {
-      goto ERROUT_INITSERVER;
-    }
-  }
+	if (type == SOCK_STREAM || type == SOCK_SEQPACKET) {
+		if (listen(fd, qlen) < 0) {
+			goto ERROUT_INITSERVER;
+		}
+	}
 
-  return(fd);
+	return (fd);
 
-ERROUT_INITSERVER:
-  err = errno;
-  close(fd);
-  errno = err;
-  return(-1);
+	ERROUT_INITSERVER: err = errno;
+	close(fd);
+	errno = err;
+	return (-1);
 }
 
 /**
@@ -53,28 +55,40 @@ ERROUT_INITSERVER:
  * @param  alen     Size of socket address
  * @return          On success returns a connected socket descriptor, on failure -1.
  */
-int connect_retry(int domain, int type, int protocol, const struct sockaddr *addr, socklen_t alen) {
-  int numsec, fd;
+int connect_retry(int domain, int type, int protocol,
+		const struct sockaddr *addr, socklen_t alen) {
+	int numsec, fd;
 
-  //
-  // Try to connect with exponential backoff.
-  //
-  for (numsec = 1; numsec <= MAXSLEEP; numsec <<= 1) {
-    if ((fd = socket(domain, type, protocol)) < 0) {
-      return(-1);
-    }
+	//
+	// Try to connect with exponential backoff.
+	//
+	for (numsec = 1; numsec <= MAXSLEEP; numsec <<= 1) {
+		if ((fd = socket(domain, type, protocol)) < 0) {
+			return (-1);
+		}
 
-    if (connect(fd, addr, alen) == 0) {
-      return(fd); // Connection accepted.
-    }
+		if (connect(fd, addr, alen) == 0) {
+			return (fd); // Connection accepted.
+		}
 
-    close(fd);
+		close(fd);
 
-    // Delay before trying again.
-    if (numsec <= MAXSLEEP / 2) {
-      sleep(numsec);
-    }
-  }
+		// Delay before trying again.
+		if (numsec <= MAXSLEEP / 2) {
+			sleep(numsec);
+		}
+	}
 
-  return(-1);
+	return (-1);
+}
+
+void signalHandler(int signum) {
+	printf("Interrupt signal; Program terminated.\n");
+
+	const char end_msg[] = "Press RETURN to exit!";
+	printf("%s\n", end_msg);
+
+	getchar();
+
+	exit(signum);
 }
